53eea2e7580f69b7f5979253e41f7ef6932ab7fb
Ismael Serrada <ismael.serrada@gmail.com>
2016-06-25 17:08:07 -0300

    Update: - Enemy Luke visual hitboxes and debug

--- Assets/Scripts/Entities/CharacterEntity.cs
+++ Assets/Scripts/Entities/CharacterEntity.cs
@@ -11,6 +11,11 @@ public class CharacterEntity : MovableEntity {
 
     protected float _direction_factor;
 
+    public BoxCollider2D attack_collider;
+    public BoxCollider2D hitzone_collider;
+
+    public eWorldDirection start_direction;
+
     protected void ChangeDirection (float dir_factor)
     {
         if (dir_factor != _direction_factor)
@@ -35,9 +40,77 @@ public class CharacterEntity : MovableEntity {
 
     public virtual eWorldDirection GetDirection() { return (eWorldDirection) _direction_factor; }
 
+    protected override void OnUpdate()
+    {
+        if (attack_collider != null)
+        {
+            attack_collider.enabled = false;
+        }
+
+        if (hitzone_collider != null)
+        {
+            hitzone_collider.enabled = false;
+        }
+
+        tk2dSpriteColliderDefinition[] sprite_colliders = _sprite.CurrentSprite.customColliders;
+        for (int i = 0; i < sprite_colliders.Length; i++)
+        {
+            tk2dSpriteColliderDefinition collider_def = sprite_colliders[i];
+            if (collider_def != null)
+            {
+                switch (collider_def.name)
+                {
+                    case "AttackCollider":
+                        if (attack_collider != null)
+                        {
+                            Vector3 origin = collider_def.origin;
+                            origin.x *= (float)GetDirection();
+                            attack_collider.offset = origin;
+                            attack_collider.size = collider_def.Size;
+                            attack_collider.enabled = true;
+                        }
+                        break;
+                    case "HitZoneCollider":
+                        if (hitzone_collider != null)
+                        {
+                            Vector3 origin = collider_def.origin;
+                            origin.x *= (float)GetDirection();
+                            hitzone_collider.offset = origin;
+                            hitzone_collider.size = collider_def.Size;
+                            hitzone_collider.enabled = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    protected override void OnStart()
+    {
+        ChangeDirection((float)start_direction);
+    }
+
+    public override void GoLeft(float factor = 1.0f)
+    {
+        base.GoLeft(factor);
+        ChangeDirection((float)eWorldDirection.left);
+    }
+
+    public override void GoRight(float factor = 1.0f)
+    {
+        base.GoRight(factor);
+        ChangeDirection((float)eWorldDirection.right);
+    }
+
+    protected override void OnLateUpdate() { }
+    protected override void OnAwake() { }
+
 #if UNITY_EDITOR
     protected override void OnDrawGizmos()
     {
+        base.OnDrawGizmos();
         if (Utils.show_gizmos)
         {
             if (move_collider != null) //Display Move Collider
@@ -51,12 +124,46 @@ public class CharacterEntity : MovableEntity {
                 Gizmos.color = new Color(0, 1, 0, 0.5f);
                 Gizmos.DrawCube(origin, size);
             }
+
+            if (_sprite != null)
+            {
+                tk2dSpriteColliderDefinition[] sprite_colliders = _sprite.CurrentSprite.customColliders;
+                for (int i = 0; i < sprite_colliders.Length; i++)
+                {
+                    tk2dSpriteColliderDefinition collider_def = sprite_colliders[i];
+                    if (collider_def != null)
+                    {
+                        Vector3 origin;
+                        Vector3 size;
+                        switch (collider_def.name)
+                        {
+                            case "AttackCollider":
+                                origin = collider_def.origin;
+                                origin.x *= (float)GetDirection();
+                                origin = transform.position + origin;
+                                origin.z -= 1;
+                                size = collider_def.Size;
+                                Gizmos.color = new Color(1, 1, 0, 0.8f);
+                                Gizmos.DrawCube(origin, size);
+                                break;
+                            case "HitZoneCollider":
+                                origin = collider_def.origin;
+                                origin.x *= (float)GetDirection();
+                                origin = transform.position + origin;
+                                origin.z -= 1;
+                                size = collider_def.Size;
+                                Gizmos.color = new Color(0, 1, 1, 0.8f);
+                                Gizmos.DrawCube(origin, size);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+               }
+            }
         }
-    }
 #endif
 
-    protected override void OnLateUpdate(){}
-    protected override void OnAwake() {}
-    protected override void OnStart() {}
-    protected override void OnUpdate() {}
+    
 }
